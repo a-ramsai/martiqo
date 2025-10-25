e4965544b58e9fb2d02fdc14bceb13e3d06e64ca
a-ramsai <a.ramsai24j@gmail.com>
2025-10-25 17:57:50 +0530

    03: Home-Layout

---
+++ src/app/(Home)/features/page.tsx
@@ -0,0 +1,6 @@
+function page() {
+  return (
+    <div>Features page</div>
+  )
+}
+export default page;
---
+++ src/app/(Home)/layout.tsx
@@ -0,0 +1,23 @@
+import Navbar from "@/components/Navbar";
+import Footer from "./Footer";
+
+
+interface Props {
+    children : React.ReactNode;
+}
+
+const layout =({children}:Props)=>
+{
+    return(
+        <div className="flex flex-col min-h-screen">
+            <Navbar/>
+            <div className="flex-1 bg-[#F4F4F0]">
+            {children}
+            </div>
+            <Footer/>
+        </div>
+    );
+}
+
+
+export default layout;
